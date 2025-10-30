0eca08ef1693d629418d47a26254b39926ae29bf
Alex Bruno <Bssallex@gmail.com>
2025-10-30 18:05:46 -0300

    feat: cria camada de controller para o serviço de usuário

---
+++ rentals/src/main/java/dev/bssallex/rentals/Controller/UserController.java
@@ -0,0 +1,52 @@
+package dev.bssallex.rentals.Controller;
+
+import dev.bssallex.rentals.Service.UserService;
+import dev.bssallex.rentals.dtos.request.UserRequest;
+import dev.bssallex.rentals.dtos.response.UserResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService service;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<UserResponse>> listAllUsers(){
+        List<UserResponse> users = service.findAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<UserResponse> getUserById(@PathVariable Long id){
+        UserResponse user = service.findUserById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<UserResponse> createUser(@RequestBody @Valid UserRequest request){
+        UserResponse created = service.createUser(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<UserResponse> updateUser(@PathVariable Long id, @RequestBody @Valid UserRequest request){
+
+        UserResponse updated = service.updateUser(id, request);
+        return ResponseEntity.ok(updated);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        service.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+}
