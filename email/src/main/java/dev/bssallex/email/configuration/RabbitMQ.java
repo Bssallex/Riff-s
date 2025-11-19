a5eb6ab321cb2e089af8e78eeda4cbb83242819b
Alex Bruno <Bssallex@gmail.com>
2025-11-19 06:47:37 -0300

    feat: cria uma fila(queue) com serializador jackson para armazenar dados via RabbitMQ

---
+++ email/src/main/java/dev/bssallex/email/configuration/RabbitMQ.java
@@ -0,0 +1,33 @@
+package dev.bssallex.email.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQ {
+
+    private final String queueName = "email-queue";
+
+    @Bean
+    public Queue queue(){
+        return new Queue(queueName, true);
+    }
+
+    @Bean
+    public MessageConverter msgConverter() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        return new Jackson2JsonMessageConverter(mapper);
+    }
+
+    // 1
+}
+
