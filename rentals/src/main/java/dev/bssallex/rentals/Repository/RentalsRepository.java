6208a20ab2ba030343dc27425c465c56ad201c08
Alex Bruno <Bssallex@gmail.com>
2025-12-27 04:37:22 -0300

    feat: cria query para listar aluguéis com base no nome do usuário

--- rentals/src/main/java/dev/bssallex/rentals/Repository/RentalsRepository.java
+++ rentals/src/main/java/dev/bssallex/rentals/Repository/RentalsRepository.java
@@ -5,12 +5,14 @@ import dev.bssallex.rentals.enums.TypeInstrument;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface RentalsRepository extends JpaRepository<Rentals, UUID> {
 
-    Optional<Rentals> findByUsers_name(String name);
+    List<Rentals> findByUsers_name(String name);
+    Optional<Rentals> findByTag(String tag);
     long countByInstruments_TypeInstrument(TypeInstrument type);
 }
